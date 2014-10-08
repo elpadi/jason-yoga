d39fff9f207945266a7dd1271eaad9fc2b13748f
carlos (v) <carlos@versionindustries.com>
2014-10-08 18:52:55 -0400

    Initial setup of the sidebar.

--- wp-content/themes/jasonyoga/index.php
+++ wp-content/themes/jasonyoga/index.php
@@ -17,55 +17,53 @@
 get_header(); ?>
 
 <div class="backgroundwrapper">
+	<div class="contentwrapper">
+		<div class="content">
 
-
-<div class="contentwrapper">
-
-	<div class="content">
-
-		<?php if ( have_posts() ) : ?>
-			
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post(); ?>
-
-				<header>
-					<p class="metadata gray-text"><?php the_date(); ?> by <span class="red-text"><?php the_author(); ?></span></p>
-					<h1><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h1>
-				</header>
-
-				<?php the_content(); ?>
-
-				<?php the_tags( '<footer class="entry-meta" style="padding: 40px 0 0; color: #cc303a;"><span class="tag-links">', ', ', '</span></footer>' ); ?>
-
+			<?php if ( have_posts() ) : ?>
+				
 				<?php
-  				 comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments are off for this post');
-				?>
+				// Start the Loop.
+				while ( have_posts() ) : the_post(); ?>
 
-				<a class="add-comment" href="<?php the_permalink(); ?>">Add Your Own</a>
+					<header>
+						<p class="metadata gray-text"><?php the_date(); ?> by <span class="red-text"><?php the_author(); ?></span></p>
+						<h1><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h1>
+					</header>
 
-				<?php wp_related_posts()?>
+					<?php the_content(); ?>
 
-				<hr>
-				<hr>
+					<?php the_tags( '<footer class="entry-meta" style="padding: 40px 0 0; color: #cc303a;"><span class="tag-links">', ', ', '</span></footer>' ); ?>
 
-			<?php endwhile; ?>
+					<?php
+						 comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments are off for this post');
+					?>
 
-		<?php endif; ?>
+					<a class="add-comment" href="<?php the_permalink(); ?>">Add Your Own</a>
 
-		<div class="pag">
-			<div class="next">
-				<?php previous_posts_link( 'Next' ); ?>
+					<?php wp_related_posts()?>
+
+					<hr>
+					<hr>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+			<div class="pag">
+				<div class="next">
+					<?php previous_posts_link( 'Next' ); ?>
+				</div>
+				<div class="previous">
+					<?php next_posts_link( 'Previous', $query_press->max_num_pages ); ?>
+				</div>
 			</div>
-			<div class="previous">
-				<?php next_posts_link( 'Previous', $query_press->max_num_pages ); ?>
-			</div>
+
 		</div>
-
+		<aside id="main-sidebar">
+			<?php echo 'sidebar';//sidebar ?>
+		</aside>
 	</div>
-
-</div>
-
 </div>
 
 <?php get_footer(); ?>
