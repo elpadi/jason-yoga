8be8659527abdaeb1c049ad920fa419a15ba4c45
carlos (el-cajon) <padi_05@yahoo.com>
2015-10-27 07:57:00 -0400

    Quote author name can now also be a link.

--- wp-content/mu-plugins/src/JY.php
+++ wp-content/mu-plugins/src/JY.php
@@ -13,7 +13,7 @@ class JY extends App {
 		$posts = get_posts(['post_type' => self::prefix('testimonials'), 'posts_per_page' => -1]);
 		$ids = F\pluck($posts, 'ID');
 		$featured_id = get_option('featured_testimonial_id', 2094);
-		$data = $this->customPostsQuery($ids, ['text' => ['testimonial_quote','author_info'], 'attachments' => ['thumb']], 'posts.`post_date` DESC');
+		$data = $this->customPostsQuery($ids, ['text' => ['testimonial_quote','author_info','author_url'], 'attachments' => ['thumb']], 'posts.`post_date` DESC');
 		array_walk($data, array($this, 'hydrateTestimonial'), $featured_id);
 		foreach ($data as $post) $by_type[$post->quote_type][] = $post;
 		return $by_type;
@@ -24,7 +24,7 @@ class JY extends App {
 			empty($post->author_info) ? 'short' : 'long'
 		); 
 		$post->author_name = get_the_title($post);
-		$post->author_info_parts = explode("\n", $post->author_info);
+		$post->author_info_parts = array_filter(explode("\n", $post->author_info));
 		if ($post->author_name === 'Jason Crandell') $post->quote_type = 'jason';
 		if (!empty($post->thumb_serialized)) {
 			$upload_dir = wp_upload_dir();
--- wp-content/themes/jasonyoga/inc/templates/quote.php
+++ wp-content/themes/jasonyoga/inc/templates/quote.php
@@ -1,7 +1,11 @@
 <article class="quote" <?php if (isset($quote->thumb)): ?>style="background-image:url(<?php echo $quote->thumb; ?>);"<?php endif; ?>>
 	<blockquote><?php echo apply_filters('the_content', '<span class="quote-mark open">“</span>'.$quote->testimonial_quote.'<span class="quote-mark close">”</span>'); ?></blockquote>
 	<footer>
+		<?php if (!empty($quote->author_url)): ?>
+		<a href="<?php echo $quote->author_url; ?>" target="_blank"><?php echo $quote->author_name; ?></a>
+		<?php else: ?>
 		<span><?php echo $quote->author_name; ?></span>
+		<?php endif; ?>
 		<?php if (!empty($quote->author_info_parts)): foreach ($quote->author_info_parts as $part): ?> <span><?php echo trim($part); ?></span> <?php endforeach; endif; ?>
 	</footer>
 </article>
